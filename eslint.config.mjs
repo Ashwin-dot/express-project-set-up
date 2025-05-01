706b10e33ceca399ac6f1ccfb1a175264f244f10
Ashwin-dot <khatiwadaashwin5@gmail.com>
2025-05-01 21:24:49 +0545

    eslint, preetier, husky

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+// @ts-check
+// @ts-check
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-console': 'warn',
+    },
+    ignores: ['node_modules', 'dist'],
+  },
+];
